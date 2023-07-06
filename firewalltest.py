2ac68c7e4ba3245f78dbd6f51dc8a5c41615f83b
Ajay Krishna K A <95010204+DRS975@users.noreply.github.com>
2023-07-06 10:48:57 +0530

    Add files via upload

---
+++ firewalltest.py
@@ -0,0 +1,39 @@
+import datetime
+
+def process_commands(input_file):
+    protocols = []
+    
+    with open(input_file, 'r') as file:
+        protocols = [line.strip() for line in file.readlines()]
+
+    command_list = []
+    last_protocol_index = min(len(protocols), 1500) - 1
+
+    a = 0
+    b = 0
+    name = input("enter name")	
+    for i, protocol in enumerate(protocols):
+        command_list.append(f"edit {name}{a}/{b}")
+        command_list.append(f"set subnet {protocol}/32")
+        command_list.append(f"command messagereceived<{datetime.datetime.now().strftime('%d%b%Y')}>")
+        if i != last_protocol_index:
+            command_list.append("next")
+        
+        b += 1
+        if b > 1500:
+            b = 0
+            a += 1
+
+    command_list.append("end")
+
+    return command_list
+
+def save_output(output_file, commands):
+    with open(output_file, 'w') as file:
+        file.write('\n'.join(commands))
+
+input_file = input("enter input file name")
+output_file = input("enter output file name")
+
+commands = process_commands(input_file)
+save_output(output_file, commands)
